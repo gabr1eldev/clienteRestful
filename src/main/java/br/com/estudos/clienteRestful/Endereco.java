353e913ecb511888c23f662b7a4a46ccd5e4c3ec
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 10:05:53 -0300

    criando class endereço

---
+++ src/main/java/br/com/estudos/clienteRestful/Endereco.java
@@ -0,0 +1,32 @@
+package br.com.estudos.clienteRestful;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Endereco implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String cep;
+	private String rua;
+	private Integer numero;
+	private String bairro;
+	private String cidade;
+	private String estado;
+
+}
