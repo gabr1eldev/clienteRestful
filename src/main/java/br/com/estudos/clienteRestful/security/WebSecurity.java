ef43aeb5b0110bdedbf53035248caacda539c1ec
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 13:13:01 -0300

    customizando o AuthenticationManager no WebSecurity

--- src/main/java/br/com/estudos/clienteRestful/security/WebSecurity.java
+++ src/main/java/br/com/estudos/clienteRestful/security/WebSecurity.java
@@ -1,20 +1,35 @@
 package br.com.estudos.clienteRestful.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.estudos.clienteRestful.service.CustomUserDetailService;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private CustomUserDetailService customUserDetailService;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.anyRequest().authenticated()
 			.and()
-			.httpBasic();
-			
+			.httpBasic()
+			.and()
+			.csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserDetailService)
+		.passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
