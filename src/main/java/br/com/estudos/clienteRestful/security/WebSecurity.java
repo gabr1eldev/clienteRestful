46871b254916d252dece2cdd13716878daae0359
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 11:39:57 -0300

    criando a class WebSecurity

---
+++ src/main/java/br/com/estudos/clienteRestful/security/WebSecurity.java
@@ -0,0 +1,8 @@
+package br.com.estudos.clienteRestful.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class WebSecurity {
+
+}
