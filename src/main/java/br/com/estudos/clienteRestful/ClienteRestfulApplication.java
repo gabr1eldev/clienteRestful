909ef82bca2f772e01af6fcc8d0ee7240a6870c7
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 09:37:54 -0300

    projeto inicial

---
+++ src/main/java/br/com/estudos/clienteRestful/ClienteRestfulApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.clienteRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudos/clienteRestful/ClienteRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.clienteRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
