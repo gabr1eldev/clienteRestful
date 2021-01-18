9ae34e52ff5223063c785d923c6f4bd2f6970470
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 10:09:49 -0300

    criando o repositório do usuario

---
+++ src/main/java/br/com/estudos/clienteRestful/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.estudos.clienteRestful.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.estudos.clienteRestful.entity.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	Usuario findByUsername(String username);
+	boolean existsByUsernameAndPassword(String username, String password);
+}
