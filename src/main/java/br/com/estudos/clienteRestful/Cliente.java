d78706d6e42ddbd3bb0888c8c3b61789388d3e41
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 10:00:17 -0300

    criando o Cliente

---
+++ src/main/java/br/com/estudos/clienteRestful/Cliente.java
@@ -0,0 +1,37 @@
+package br.com.estudos.clienteRestful;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String cpf;
+	
+	private String nome;
+	private String rg;
+	private Integer idade;
+	
+	private LocalDate dataCadastro;
+	
+	void preDataCadastro() {
+		setDataCadastro(LocalDate.now());
+		
+	}
+
+}
