2fe233a878eeb7d5c2e05e8972c94869cf138c65
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 13:07:41 -0300

    customizando o loadUserByUsername

--- src/main/java/br/com/estudos/clienteRestful/service/CustomUserDetailService.java
+++ src/main/java/br/com/estudos/clienteRestful/service/CustomUserDetailService.java
@@ -1,10 +1,16 @@
 package br.com.estudos.clienteRestful.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import br.com.estudos.clienteRestful.entity.Usuario;
 import br.com.estudos.clienteRestful.repository.UsuarioRepository;
 
 @Component
@@ -20,8 +26,16 @@ public class CustomUserDetailService implements UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Usuario usuario = Optional.ofNullable(usuarioRepository.findByUsername(username))
+		.orElseThrow(() -> new UsernameNotFoundException("Login não encontrado"))  ;
+		
+		List<GrantedAuthority> authorityListAdmin	=	AuthorityUtils.createAuthorityList("ROLE_USER","ROLE_ADMIN");
+		List<GrantedAuthority> authorityListUser	=	AuthorityUtils.createAuthorityList("ROLE_USER");
+		
+		return new org.springframework.security.core.userdetails.User(
+				usuario.getUsername(),
+				usuario.getUsername(),
+				usuario.isAdmin() ? authorityListAdmin : authorityListUser);
 	}
 
 }
