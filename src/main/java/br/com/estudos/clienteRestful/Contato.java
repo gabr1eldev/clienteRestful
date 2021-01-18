8f071c4aca83cdd5478aa802b40375e9965400e8
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 10:03:52 -0300

    criando class Contato

--- src/main/java/br/com/estudos/clienteRestful/Contato.java
+++ src/main/java/br/com/estudos/clienteRestful/Contato.java
@@ -1,5 +1,29 @@
 package br.com.estudos.clienteRestful;
 
-public class Contato {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Contato implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String numero;
+	
+	
 
 }
