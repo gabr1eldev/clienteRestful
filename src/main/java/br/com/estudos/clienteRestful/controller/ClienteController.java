99eb569d723979d0258aefbe7472cd113d39a366
unknown <gabriel.gregorio1987@gmail.com>
2021-01-18 10:56:11 -0300

    criando end-point procurarTodos do Cliente

--- src/main/java/br/com/estudos/clienteRestful/controller/ClienteController.java
+++ src/main/java/br/com/estudos/clienteRestful/controller/ClienteController.java
@@ -1,12 +1,16 @@
 package br.com.estudos.clienteRestful.controller;
 
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.estudos.clienteRestful.entity.Cliente;
 import br.com.estudos.clienteRestful.repository.ClienteRepository;
 
@@ -29,5 +33,12 @@ public class ClienteController {
 		return new ResponseEntity<Cliente>(salvo,HttpStatus.OK);
 	}
 	
+	@GetMapping
+	public ResponseEntity<List<Cliente>> procurarTodos() {
+		
+		List<Cliente> cliente = clienteRepository.findAll();
+		
+		return new ResponseEntity<List<Cliente>>(cliente,HttpStatus.OK);
+	}
 
 }
